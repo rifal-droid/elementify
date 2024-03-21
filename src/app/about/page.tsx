bc419031ec2d6108f56cccecce6c6620f2b5dab2
Della Rahmatika <dellarahmatika2@gmail.com>
2024-03-21 16:04:17 +0700

    Menambah about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+        <div>
+            <p>
+              Hello World, this is about page
+            </p>
+        </div>
+  );
+}
